15a1e27917cea1c1745cdf0172a0a14868830084
elisabetaOnoriu <elisabeta.onoriu99@gmail.com>
2022-11-26 14:00:43 +0200

    Added functions Integer and IsInteger

--- Json.Validator/JsonNumber.cs
+++ Json.Validator/JsonNumber.cs
@@ -6,7 +6,19 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            throw new NotImplementedException();
+            var indexOfDot = input.IndexOf('.');
+            var indexOfExponent = input.IndexOfAny("eE".ToCharArray());
+            return IsInteger(Integer(input, indexOfDot, indexOfExponent));
+        }
+
+        private static string Integer(string input, int indexOfDot, int indexOfExponent)
+        {
+            return indexOfDot == -1 && indexOfExponent == -1 ? input : string.Empty;
+        }
+
+        private static bool IsInteger(string integer)
+        {
+            return int.TryParse(integer, out var result);
         }
     }
 }
