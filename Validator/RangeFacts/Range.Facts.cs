5f80bc982c370f41173c287ea43f18672a1c92d1
elisabetaOnoriu <elisabeta.onoriu99@gmail.com>
2022-12-08 10:44:21 +0200

    Refactored RangeFacts - splitted tests in multiple tests

---
+++ Validator/RangeFacts/Range.Facts.cs
@@ -0,0 +1,50 @@
+global using Xunit;
+
+namespace Validator.RangeFacts
+{
+    public class TestProgram
+    {
+        [Theory]
+        [InlineData('b', 'i', "bizar")]
+        [InlineData('1', '5', "1marinar")]
+        public void IsInRange_TrueCases(char start, char end, string text)
+        {
+            var result = new Range(start, end);
+            Assert.True(result.Match(text));
+        }
+
+        [Theory]
+        [InlineData(' ', '/', "*code")]
+        public void IsInRange_OddCharacters_TrueCases(char start, char end, string text)
+        {
+            var result = new Range(start, end);
+            Assert.True(result.Match(text));
+        }
+
+        [Theory]
+        [InlineData('c', 'e', "whale")]
+        [InlineData('a', 'z', "1234")]
+        public void IsInRange_FalseCases(char start, char end, string text)
+        {
+            var result = new Range(start, end);
+            Assert.False(result.Match(text));
+        }
+
+        [Theory]
+        [InlineData('#', '@', "~abc")]
+        public void IsInRange_OdCharacters_FalseCases(char start, char end, string text)
+        {
+            var result = new Range(start, end);
+            Assert.False(result.Match(text));
+        }
+
+        [Theory]
+        [InlineData('a', 'z', null)]
+        [InlineData('a', 'z', "")]
+        public void IsInRange__IsNotNullOrEmpty_FalseCases(char start, char end, string text)
+        {
+            var result = new Range(start, end);
+            Assert.False(result.Match(text));
+        }
+    }
+}
