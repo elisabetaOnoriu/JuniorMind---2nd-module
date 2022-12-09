51b4e1a12508a50582af5701e89f86d73bd22130
elisabetaOnoriu <elisabeta.onoriu99@gmail.com>
2022-12-09 10:08:34 +0200

    Refactored Match in Choice - removed useless optimization

--- Validator/Validator/Character.cs
+++ Validator/Validator/Character.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Validator
+﻿namespace Validator
 {
     public class Character : IPattern
     {
--- Validator/Validator/Choice.cs
+++ Validator/Validator/Choice.cs
@@ -12,15 +12,10 @@ namespace Validator
 
         public IMatch Match(string text)
         {
-            if(string.IsNullOrEmpty(text))
-            {
-                return new FailedMatch(text);
-            }
-
             foreach (var pattern in patterns)
             {
                 var match = pattern.Match(text);
-                if (match.Success())
+                if (!string.IsNullOrEmpty(text) && match.Success())
                 {
                     return new SuccessMatch(text[1..]);
                 }
