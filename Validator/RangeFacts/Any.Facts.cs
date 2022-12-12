ea587058e03908976342d7afe04436b3e04c23f9
elisabetaOnoriu <elisabeta.onoriu99@gmail.com>
2022-12-12 22:23:17 +0200

    Added Any Class with its tests

--- Validator/RangeFacts/Any.Facts.cs
+++ Validator/RangeFacts/Any.Facts.cs
@@ -2,7 +2,37 @@
 
 namespace Validator.AnyFacts
 {
-    internal class Any
+    public class TestProgram
     {
+        [Theory]
+        [InlineData("eE", "e57")]
+        [InlineData("eE", "Eva")]
+        public void TextStartsWithAnyAcceptedItem_TrueCases(string accepted, string text)
+        {
+            var possibilities = new Any(accepted);
+            var match = possibilities.Match(text);
+            Assert.True(match.Success());
+            Assert.Equal(text[1..], match.RemainingText());
+        }
+
+        [Fact]
+        public void TextStartsWithAnyAcceptedItem_FalseCases()
+        {
+            var possibilities = new Any("Uu");
+            var match = possibilities.Match("bbc");
+            Assert.False(match.Success());
+            Assert.Equal("bbc", match.RemainingText());
+        }
+
+        [Theory]
+        [InlineData("eE", "")]
+        [InlineData("eE", null)]
+        public void TextStartsWithAnyAcceptedItem_FalseCases_NullOrEmptyCases(string accepted, string text)
+        {
+            var possibilities = new Any(accepted);
+            var match = possibilities.Match(text);
+            Assert.False(match.Success());
+            Assert.Equal(text, match.RemainingText());
+        }
     }
 }
--- Validator/Validator/Any.cs
+++ Validator/Validator/Any.cs
@@ -1,6 +1,22 @@
 ﻿namespace Validator
 {
-    class Any
+    public class Any : IPattern
     {
+        readonly string accepted;
+
+        public Any(string accepted)
+        {
+            this.accepted = accepted;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new FailedMatch(text);
+            }
+
+            return accepted.Contains(text[0]) ? new SuccessMatch(text[1..]) : new FailedMatch(text);
+        }
     }
 }
