59910b292ef787091b8d5af9bfe1d526fd89f811
elisabetaOnoriu <elisabeta.onoriu99@gmail.com>
2022-12-13 10:19:05 +0200

    Added Many and its tests

---
+++ Validator/RangeFacts/Many.Facts.cs
@@ -0,0 +1,32 @@
+﻿global using Xunit;
+
+namespace Validator.ManyFacts
+{
+    public class TestProgram
+    {
+        [Theory]
+        [InlineData("iiiijk", 'i')]
+        [InlineData("ijk", 'i')]
+
+        public void StringContainsThePatternAtLeastOnce_TrueCases(string text, char a)
+        {
+            var pattern = new Many(new Character(a));
+            var match = pattern.Match(text);
+            Assert.True(match.Success());
+            Assert.Equal(text[text.IndexOf('j')..], match.RemainingText());
+        }
+
+        [Theory]
+        [InlineData("", 'i')]
+        [InlineData(null, 'i')]
+        [InlineData("abc", 'd')]
+
+        public void StringDoesNotContainThePattern_TrueCases(string text, char a)
+        {
+            var pattern = new Many(new Character(a));
+            var match = pattern.Match(text);
+            Assert.True(match.Success());
+            Assert.Equal(text, match.RemainingText());
+        }
+    }
+}
--- Validator/Validator/Many.cs
+++ Validator/Validator/Many.cs
@@ -1,6 +1,25 @@
 ﻿namespace Validator
 {
-    public class Many
+    public class Many : IPattern
     {
+        readonly IPattern pattern;
+
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            IMatch match = new SuccessMatch(text);
+            while (match.Success())
+            {
+                match = pattern.Match(match.RemainingText());
+            }
+
+            return match.RemainingText() == text ?
+                new SuccessMatch(text) :
+                new SuccessMatch(match.RemainingText());
+        }
     }
 }
