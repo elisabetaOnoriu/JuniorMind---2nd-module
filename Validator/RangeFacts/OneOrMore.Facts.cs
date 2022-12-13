96052df50c5719237836882f762913845c4838db
elisabetaOnoriu <elisabeta.onoriu99@gmail.com>
2022-12-13 14:20:03 +0200

    Added OneOrMore and its tests

---
+++ Validator/RangeFacts/OneOrMore.Facts.cs
@@ -0,0 +1,40 @@
+﻿global using Xunit;
+
+namespace Validator.OneOrMoreFacts
+{
+    public class TestProgram
+    {
+        [Fact]
+        public void TextStartsWithTheGivenCharacter_ConsumesWholeText()
+        {
+            var text = "123";
+            var pattern = new OneOrMore(new Range('1', '9'));
+            var match = pattern.Match(text);
+            Assert.True(match.Success());
+            Assert.Equal(text[3..], match.RemainingText());
+        }
+
+        [Fact]
+        public void TextStartsWithTheGivenCharacter_TrueCases()
+        {
+            var text = "1a";
+            var pattern = new OneOrMore(new Range('1', '9'));
+            var match = pattern.Match(text);
+            Assert.True(match.Success());
+            Assert.Equal(text[1..], match.RemainingText());
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("bc")]
+
+        public void TextDoesNotStartWithTheGivenCharacter_TrueCases(string text)
+        {
+            var pattern = new OneOrMore(new Range('1', '9'));
+            var match = pattern.Match(text);
+            Assert.False(match.Success());
+            Assert.Equal(text, match.RemainingText());
+        }
+    }
+}
---
+++ Validator/Validator/OneOrMore.cs
@@ -0,0 +1,18 @@
+﻿namespace Validator
+{
+    public class OneOrMore : IPattern
+    {
+        readonly IPattern pattern;
+
+        public OneOrMore(IPattern pattern)
+        {
+            this.pattern = new Many(pattern);
+        }
+
+        public IMatch Match(string text)
+        {
+            var match = pattern.Match(text);
+            return match.RemainingText() == text ? new FailedMatch(text) : match;
+        }
+    }
+}
