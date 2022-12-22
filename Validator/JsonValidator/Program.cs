e396393367bfeac8319745c8114f31a6b84111a6
elisabetaOnoriu <elisabeta.onoriu99@gmail.com>
2022-12-22 12:17:10 +0200

    Refactored console aplication to show a message when arguments are not being passed

--- Validator/JsonValidator/Program.cs
+++ Validator/JsonValidator/Program.cs
@@ -6,6 +6,12 @@ namespace JsonValidator
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Pass in the file path as an argument");
+                return;
+            }
+
             string text = System.IO.File.ReadAllText(args[0]);
             var jsonText = new Value();
             var match = jsonText.Match(text);
