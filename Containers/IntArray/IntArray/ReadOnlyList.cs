29a32cf92e43e5f2f2d64a99b2624b786ddb6232
elisabetaOnoriu <elisabeta.onoriu99@gmail.com>
2023-02-16 12:26:21 +0200

    Changed methods to return as expressions

--- Containers/IntArray/IntArray/ReadOnlyList.cs
+++ Containers/IntArray/IntArray/ReadOnlyList.cs
@@ -11,10 +11,7 @@ namespace Collections
             this.list = list; 
         }
 
-        public void Add(T item)
-        {
-            ThrowExceptionIfListIsReadonly();
-        }
+        public void Add(T item) => ThrowExceptionIfListIsReadonly();
 
         public int Count { get => list.Count; }
 
@@ -26,50 +23,27 @@ namespace Collections
             set => ThrowExceptionIfListIsReadonly();
         }
 
-        public int IndexOf(T item)
-        {
-            return list.IndexOf(item);
-        }
+        public int IndexOf(T item) => list.IndexOf(item);
 
-        public bool Contains(T item)
-        {
-            return list.Contains(item);
-        }
+        public bool Contains(T item) => list.Contains(item);
 
-        public void Insert(int index, T item)
-        {
-            ThrowExceptionIfListIsReadonly();
-        }
+        public void Insert(int index, T item) => ThrowExceptionIfListIsReadonly();
 
-        public void Clear()
-        {
-            ThrowExceptionIfListIsReadonly();
-        }
-        public bool Remove(T item)
+        public void Clear() => ThrowExceptionIfListIsReadonly();
+
+        public bool Remove(T item) 
         {
             ThrowExceptionIfListIsReadonly();
             return false;
         }
 
-        public void RemoveAt(int index)
-        {
-            ThrowExceptionIfListIsReadonly();
-        }
+        public void RemoveAt(int index) => ThrowExceptionIfListIsReadonly();
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            return list.GetEnumerator();
-        }
+        public IEnumerator<T> GetEnumerator() => list.GetEnumerator();
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-        public void CopyTo(T[] array, int arrayIndex)
-        {
-            list.CopyTo(array, arrayIndex);
-        }
+        public void CopyTo(T[] array, int arrayIndex) => list.CopyTo(array, arrayIndex);
 
         private void ThrowExceptionIfListIsReadonly()
         {
