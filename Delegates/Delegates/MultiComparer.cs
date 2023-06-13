5fdde4a2ce0ff37b234ebe30efd53a70337b4d09
elisabetaOnoriu <elisabeta.onoriu99@gmail.com>
2023-06-13 14:50:11 +0300

    Removed additional classes

---
+++ Delegates/Delegates/MultiComparer.cs
@@ -0,0 +1,27 @@
+﻿namespace Delegates
+{
+    internal class MultiComparer<TSource, TKey> : IComparer<TSource>
+    {
+        internal IComparer<TKey> actual;
+        internal IComparer<TSource> next;
+        Func<TSource, TKey> keySelector;
+
+        internal MultiComparer(IComparer<TKey> comparer, Func <TSource, TKey> keySelector)
+        {
+            actual = comparer;
+            this.keySelector = keySelector;
+            next = null;
+        }
+
+        public int Compare(TSource? x, TSource? y)
+        {
+            int compared = actual.Compare(keySelector(x), keySelector(y));
+            if (compared != 0)
+            {
+                compared = next.Compare(x, y);
+            }
+
+            return compared;
+        }
+    }
+}
--- Delegates/Delegates/OrderedEnumerable.cs
+++ Delegates/Delegates/OrderedEnumerable.cs
@@ -1,26 +1,51 @@
-﻿namespace Delegates
+﻿using System.Collections;
+namespace Delegates
 {
-    internal class OrderedEnumerable<TElement, TKey> : OrderedEnum<TElement>
+    internal class OrderedEnumerable<TSource, TKey> : IOrderedEnumerable<TSource>
     {
-        internal OrderedEnum<TElement> parent;
-        internal Func<TElement, TKey> keySelector;
-        internal IComparer<TKey> comparer;
+        internal IEnumerable<TSource> source;
+        internal Func<TSource, TKey> keySelector;
+        internal IComparer<TSource> comparer;
+        internal MultiComparer<TSource, TKey> multiComparer;
         internal bool descending;
 
-        internal OrderedEnumerable(IEnumerable<TElement> source, Func<TElement, TKey> keySelector, IComparer<TKey> comparer, bool descending)
+        internal OrderedEnumerable(IEnumerable<TSource> source, Func<TSource, TKey> keySelector, Comparer<TSource> comparer, bool descending)
         {
-            parent = null;
             this.source = source;
             this.keySelector = keySelector;
-            this.comparer = comparer != null ? comparer : Comparer<TKey>.Default;
+            this.comparer = comparer;
             this.descending = descending;
+            this.multiComparer = new MultiComparer<TSource, TKey>(comparer, keySelector);
         }
 
-        internal override SourceSorter<TElement> GetSourceSorter(SourceSorter<TElement> next)
+        public IOrderedEnumerable<TSource> CreateOrderedEnumerable<TKey1>(Func<TSource, TKey1> keySelector1, IComparer<TKey1>? comparer1, bool descending)
         {
-            SourceSorter<TElement> sorter = new SourceSorter<TElement, TKey>(keySelector, comparer, descending, next);
-            if (parent != null) sorter = parent.GetSourceSorter(sorter);
-            return sorter;
+            multiComparer.next = comparer1;
+            return new OrderedEnumerable<TSource, TKey1>(source, keySelector1, comparer1, descending);
         }
+
+        public IEnumerator<TSource> GetEnumerator()
+        {
+            List<TSource> list = new(source);
+            InsertionSort(list);
+            for (int i = 0; i < list.Count; i++)
+            {
+                yield return list[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        void InsertionSort(List<TSource> list)
+        {
+            for (int i = 1; i < list.Count; i++)
+            {
+                for (int j = i; descending ? j >= 0 : j > 0 && comparer.Compare(list[j - 1], list[j]) == (descending ? 1 : -1); j--)
+                {
+                    (list[j - 1], list[j]) = (list[j], list[j - 1]);
+                }
+            }
+        }
+
     }
 }
