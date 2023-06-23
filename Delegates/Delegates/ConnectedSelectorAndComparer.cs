393c4ea93680f51bba4b7e372f88214b3a0726db
elisabetaOnoriu <elisabeta.onoriu99@gmail.com>
2023-06-23 11:40:39 +0300

    Renamed field

--- Delegates/Delegates/ConnectedSelectorAndComparer.cs
+++ Delegates/Delegates/ConnectedSelectorAndComparer.cs
@@ -2,15 +2,15 @@
 {
     internal class ConnectedSelectorAndComparer<TSource, TKey> : IComparer<TSource>
     {
-        readonly Func<TSource, TKey> projection;
+        readonly Func<TSource, TKey> keySelector;
         readonly IComparer<TKey> comparer;
 
-        public ConnectedSelectorAndComparer(Func<TSource, TKey> projection, IComparer<TKey> comparer)
+        public ConnectedSelectorAndComparer(Func<TSource, TKey> keySelector, IComparer<TKey> comparer)
         {
             this.comparer = comparer ?? Comparer<TKey>.Default;
-            this.projection = projection;
+            this.keySelector = keySelector;
         }
 
-        public int Compare(TSource x, TSource y) => comparer.Compare(projection(x), projection(y));
+        public int Compare(TSource x, TSource y) => comparer.Compare(keySelector(x), keySelector(y));
     }
 }
