fab747fdb7799eaeed2a9be6606f5117741b9338
elisabetaOnoriu <elisabeta.onoriu99@gmail.com>
2023-07-24 16:43:54 +0300

    Optimized functions

--- Delegates/Queries/Queries.cs
+++ Delegates/Queries/Queries.cs
@@ -4,55 +4,47 @@
     {
         public static (int, int) CountVowelsAndConsonant(this string input)
         {
-            int consonantsCount = input.Where(c => c.IsConsonant()).Count();
-            int vowelsCount = input.Where(c => c.IsVowel()).Count();
-            return (vowelsCount, consonantsCount);
+            return input.Aggregate((0, 0), (counts, c) =>
+            {
+                if (char.IsLetter(c))
+                {
+                    return IsVowel(c)
+                        ? (counts.Item1 + 1, counts.Item2)
+                        : (counts.Item1, counts.Item2 + 1);
+                }
+                return counts;
+            });
         }
 
-        public static char FindFirstUniqueChar(this string input) => input.First(a => input.IndexOf(a) == input.LastIndexOf(a));
+        public static char FindFirstUniqueChar(this string input) => input.Distinct().First();
 
-        public static int StringToInt(this string input) => input.Aggregate(0, (number, next) => number * 10 + (next - '0'));
+        public static int StringToInt(this string input) => input.Aggregate(0, (number, next) => char.IsDigit(next) ? 
+                                                            number * 10 + (next - '0') : throw new FormatException());
 
         public static char GetsRepeatedTheMost(this string input) => input.GroupBy(c => c).MaxBy(g => g.Count()).Key;
 
-        public static string[] Palindromes(this string input)
+        public static IEnumerable<string> Palindromes(this string input)
         {
             return Enumerable
                 .Range(1, input.Length)
                 .SelectMany(length => Enumerable.Range(0, input.Length - length + 1)
                                                 .Select(a => input.Substring(a, length)))
-                                                .Where(b => b.SequenceEqual(b.Reverse()))
-                                                .ToArray();
+                                                .Where(b => b.SequenceEqual(b.Reverse()));
         }
 
-        public static List<List<int>> SubarraySum(this int[] array, int k)
+        public static IEnumerable<IEnumerable<int>> SubarraySum(this int[] array, int k)
         {
             return Enumerable
-                .Range(0, array.Length)
+            .Range(0, array.Length)
                 .SelectMany(i => Enumerable.Range(i, array.Length - i)
-                .Aggregate(new List<List<int>>(), (acc, j) =>
-                {
-                    var subarray = array.Skip(i).Take(j - i + 1).ToList();
-                    if (subarray.Sum() <= k)
-                    {
-                        acc.Add(subarray);
-                    }
-
-                    return acc;
-                }))
-                .ToList();
-        }
-
-        static private bool IsConsonant(this char c)
-        {
-            var consonants = new HashSet<char>() { 'b', 'c', 'd', 'f', 'g', 'h', 'j',
-                'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
-            return consonants.Contains(char.ToLower(c));
+                .Where(j => array.Skip(i).Take(j - i + 1).Sum() <= k)
+                .Select(j => array.Skip(i).Take(j - i + 1))
+            );
         }
 
         static private bool IsVowel(this char c)
         {
-            var vowels = new HashSet<char>() { 'a', 'e', 'i', 'o', 'u' };
+            var vowels = new HashSet<char>("aeiou");
             return vowels.Contains(char.ToLower(c));
         }
     }
